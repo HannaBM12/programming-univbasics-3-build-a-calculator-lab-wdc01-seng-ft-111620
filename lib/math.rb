de31f2db7bccce2a1013a49fc34a4a4aac2d682c
Hanna Mulugeta <hannabmulugeta@gmail.com>
2020-10-12 04:22:44 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -8,12 +8,13 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  product = num1*num2
-  product
+  div = num2 / num1
+  div
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  product
 end
 
 def modulo(num1, num2)
